63ebb6909d2499403e6cdc02e0b5d7660092218b
Carlos Mendes <carlosmendes_7@hotmail.com>
2018-06-20 15:00:19 -0300

    Update imprimeInt.asm

--- imprimeInt.asm
+++ imprimeInt.asm
@@ -48,8 +48,8 @@
 %endmacro
 
 section .bss
-    stackpointer resd 1
-    res          resd 1
+    stackpointer resd 1 ;usada na funcao imprimeInt
+    res          resd 1 ;usada na funcao imprimeInt
     
 section .data
     newline: dd 10
